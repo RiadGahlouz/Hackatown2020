90eda06d17e5e0a28367c9041d472f30a7dd35d9
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-01-18 13:52:44 -0500

    Small fixes and utils

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -8,7 +8,7 @@ pub fn index(state: State) -> (State, impl IntoResponse) {
 }
 
 pub fn restos(state: State) -> (State, impl IntoResponse) {
-    (state, (mime::APPLICATION_JSON, "[{\"name\":\"bobinette\", \"hours\": \"7 a 9\", \"location\": \"6e pavillon principal\"}]"))
+    (state, (mime::APPLICATION_JSON, "[{\"id\":\"0x3f43\",\"name\":\"bobinette\", \"hours\": \"7 a 9\", \"location\": \"6e pavillon principal\"}]"))
 }
 
 pub fn create_router() -> Router {
