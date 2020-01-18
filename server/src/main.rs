25e12eca12994911835b56a90cb174ab11393927
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-01-18 14:42:26 -0500

    Add restos

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
 mod routes;
+mod model;
 
 use gotham::state::State;
 use std::env;
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -2,18 +2,32 @@ use gotham::router::Router;
 use gotham::router::builder::*;
 use gotham::state::State;
 use gotham::handler::IntoResponse;
+use gotham::middleware::state::StateMiddleware;
+use gotham::pipeline::single_middleware;
+use gotham::pipeline::single::single_pipeline;
+use crate::model::AppData;
+use gotham::state::FromState;
+
 
 pub fn index(state: State) -> (State, impl IntoResponse) {
     (state, (mime::TEXT_HTML, include_str!("resources/index.html")))
 }
 
 pub fn restos(state: State) -> (State, impl IntoResponse) {
-    (state, (mime::APPLICATION_JSON, "[{\"id\":\"0x3f43\",\"name\":\"bobinette\", \"hours\": \"7 a 9\", \"location\": \"6e pavillon principal\"}]"))
+    let json_str = {
+        let state = AppData::borrow_from(&state);
+        let restos = state.restos.lock().unwrap();
+        serde_json::to_string(&*restos).unwrap()
+    };
+    (state, (mime::APPLICATION_JSON, json_str))
 }
 
 pub fn create_router() -> Router {
-    build_simple_router(|route| {
+    let state = AppData::new();
+    let (chain, pipeline) = single_pipeline(single_middleware(StateMiddleware::new(state)));
+
+    build_router(chain, pipeline, |route| {
         route.get("/").to(index);
-        route.get("/resto").to(restos);
+        route.get("/restos").to(restos);
     })
 }
