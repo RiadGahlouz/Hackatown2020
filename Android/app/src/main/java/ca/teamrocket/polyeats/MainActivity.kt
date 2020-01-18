4578e037daa5402ab1a15fab74d1741966823b73
Rose Hirigoyen <rosehirigoyen@gmail.com>
2020-01-18 14:18:16 -0500

    RH: PolyYEET

--- Android/app/src/main/java/ca/teamrocket/polyeats/MainActivity.kt
+++ Android/app/src/main/java/ca/teamrocket/polyeats/MainActivity.kt
@@ -1,14 +1,35 @@
 package ca.teamrocket.polyeats
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import ca.teamrocket.polyeats.historyFragment.HistoryFragment
+import ca.teamrocket.polyeats.historyFragment.transaction.TransactionContent
+import ca.teamrocket.polyeats.restoFragment.RestoFragment
+import ca.teamrocket.polyeats.restoFragment.resto.RestoContent
+import ca.teamrocket.polyeats.searchFragment.SearchFragment
+import ca.teamrocket.polyeats.searchFragment.suggestion.SearchContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    HistoryFragment.OnListFragmentInteractionListener,
+    RestoFragment.OnListFragmentInteractionListener,
+    SearchFragment.OnListFragmentInteractionListener{
+    override fun onListFragmentInteraction(item: SearchContent.SuggestionItem?) {
+        Log.d("SEARCH", "click")
+    }
+
+    override fun onListFragmentInteraction(item: RestoContent.RestoItem?) {
+        Log.d("RESTO", "click")
+    }
+
+    override fun onListFragmentInteraction(item: TransactionContent.TransactionItem?) {
+        Log.d("HISTORY", "click")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +37,11 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
+        // Passing each menu ID as a set of Ids because each`
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_resto, R.id.navigation_search, R.id.navigation_history
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
